2a6a5030410ddd79b5f16a83f892754e424ad193
rangasnafu - Lab 101 <135566054+rangasnafu@users.noreply.github.com>
2023-10-11 15:24:18 +1100

    touch to move

---
+++ Assets/Scripts/ButtonMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float horizontalBoundary = 3;
+
+    public GameObject player;
+
+    //public GameObject gameOverUI;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Left()
+    {
+        
+    }
+
+    public void Right()
+    {
+        //PlayerController.playerController()
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed;
-    public float horizontalBoundary = 22;
+    public float horizontalBoundary = 3;
 
     public GameObject gameOverUI;
+
+    public bool moveLeft = false;
+    public bool moveRight = false;
 
 
     // Start is called before the first frame update
@@ -20,27 +23,97 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateMovement();
+        UpdateMovement1();
+        UpdateMovement2();
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene("SampleScene");
             Time.timeScale = 1f;
         }
+
+        if (moveLeft)
+        {
+            if (transform.position.x >= -horizontalBoundary)
+            {
+                transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+            }
+            else
+            {
+                moveLeft = false;
+            }
+        }
+        if (moveRight)
+        {
+            if (transform.position.x < horizontalBoundary)
+            {
+                transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+            }
+            else
+            {
+                moveRight = false;
+            }
+        }
     }
 
-    private void UpdateMovement()
+    public void UpdateMovement1()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (Input.touchSupported)
+        {
+            if (Input.GetTouch(0).position.x <= Screen.width / 2f)
+            {
+                moveLeft = true;
+            }
+            else
+            {
+                moveLeft = false;
+            }
+        }
+        else
+        {
+            if (Input.mousePosition.x <= Screen.width / 2f && Input.GetMouseButton(0))
+            {
+                moveLeft = true;
+            }
+            else
+            {
+                moveLeft = false;
+            }
+        }
+    }
 
-        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary)
+    public void UpdateMovement2()
+    {
+        //float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (Input.touchSupported)
         {
-            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+            if (Input.GetTouch(0).position.x >= Screen.width / 2f)
+            {
+                moveRight = true;
+            }
+            else
+            {
+                moveRight = false;
+            }
         }
-        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary)
+        else
         {
-            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+            if (Input.mousePosition.x >= Screen.width / 2f && Input.GetMouseButton(0))
+            {
+                moveRight = true;
+            }
+            else
+            {
+                moveRight = false;
+            }
         }
+    }
+
+    public void StopMovement()
+    {
+        moveLeft = false;
+        moveRight = false;
     }
 
     private void OnTriggerEnter(Collider collision)
