cd4a93e9701574670d85aa29b3f17f0180abf33e
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-10-13 01:22:26 +1100

    timer colours + main menu

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -41,51 +41,76 @@ public class Timer : MonoBehaviour
             currentTime += Time.deltaTime;
             countdownText.text = currentTime.ToString("0");
 
-            if (currentTime >= 20)
-            {
-                countdownText.color = Color.yellow;
-            }
-
-            //if (currentTime < 0)
-            //{
-            //    Debug.Log("GameOver!");
-            //    Time.timeScale = 0f;
-            //
-            //}
-
             if (gameOverUI.activeSelf && currentTime <= 10)
             {
                 gradeFUI.SetActive(true);
             }
 
-            if (gameOverUI.activeSelf && currentTime >= 11 && currentTime <= 20)
+            if (gameOverUI.activeSelf && currentTime >= 11 && currentTime <= 30)
             {
                 gradeEUI.SetActive(true);
             }
 
-            if (gameOverUI.activeSelf && currentTime >= 21 && currentTime <= 30)
+            if (gameOverUI.activeSelf && currentTime >= 31 && currentTime <= 50)
             {
                 gradeDUI.SetActive(true);
             }
 
-            if (gameOverUI.activeSelf && currentTime >= 31 && currentTime <= 40)
+            if (gameOverUI.activeSelf && currentTime >= 51 && currentTime <= 60)
             {
                 gradeCUI.SetActive(true);
             }
 
-            if (gameOverUI.activeSelf && currentTime >= 41 && currentTime <= 50)
+            if (gameOverUI.activeSelf && currentTime >= 61 && currentTime <= 80)
             {
                 gradeBUI.SetActive(true);
             }
 
-            if (gameOverUI.activeSelf && currentTime >= 51 && currentTime <= 60)
+            if (gameOverUI.activeSelf && currentTime >= 81 && currentTime <= 100)
             {
                 gradeAUI.SetActive(true);
             }
 
-            if (gameOverUI.activeSelf && currentTime >= 61)
+            if (gameOverUI.activeSelf && currentTime >= 101)
             {
                 gradeSUI.SetActive(true);
+            }
+
+            //////////////////////////////////////
+            
+            if (currentTime <= 9)
+            {
+                countdownText.color = Color.magenta;
+            }
+
+            if (currentTime >= 10 && currentTime <= 29)
+            {
+                countdownText.color = Color.blue;
+            }
+
+            if (currentTime >= 30 && currentTime <= 49)
+            {
+                countdownText.color = Color.cyan;
+            }
+
+            if (currentTime >= 50 && currentTime <= 59)
+            {;
+                countdownText.color = Color.green;
+            }
+
+            if (currentTime >= 60 && currentTime <= 79)
+            {
+                countdownText.color = Color.yellow;
+            }
+
+            if (currentTime >= 80 && currentTime <= 99)
+            {
+                countdownText.color = Color.red;
+            }
+
+            if (currentTime >= 100)
+            {
+                countdownText.color = Color.red;
             }
         }
 
